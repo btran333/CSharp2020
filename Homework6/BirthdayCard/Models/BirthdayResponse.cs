b08e2413421efa2a3e77ea01bb23d755223a0f94
btran333 <btran333@gmail.com>
2020-05-14 17:27:32 -0700

    Homework 6

---
+++ Homework6/BirthdayCard/Models/BirthdayResponse.cs
@@ -0,0 +1,24 @@
+﻿//Bao Tran
+//Homework 6
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCard.Models
+{
+    public class BirthdayResponse
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string FromName { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string ToName { get; set; }
+
+        [Required(ErrorMessage = "Please enter Message")]
+        public string Message { get; set; }
+        public bool? CardSent { get; set; }
+    }
+}
