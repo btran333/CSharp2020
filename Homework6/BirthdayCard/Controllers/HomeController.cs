977286434d1a87ccd66f5e25d2f7eb662006b043
btran333 <btran333@gmail.com>
2020-05-14 17:33:41 -0700

    Homework6

---
+++ Homework6/BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿//Bao Tran
+//Homework 6
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(Models.BirthdayResponse sentReponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SentReponse", sentReponse);
+            }
+            else
+            {
+                return View();
+            }            
+        }
+
+
+    }
+}
