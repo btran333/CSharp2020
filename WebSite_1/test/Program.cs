fcf45976484c5c6f6600364622f305d3144db882
btran333 <btran333@gmail.com>
2020-08-08 11:04:04 -0700

    ASP.NET Project 1

---
+++ WebSite_1/test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Database accessor. This opens the database automatically
+            var school = new ClassEntities();
+
+            // This accesses the ClassMaster table
+            foreach (var classMaster in school.Classes)
+            {
+                Console.WriteLine("ClassId: {0}\nClassName: {1}\nClassDescription: {2}\nClassPrice: {3}\n",
+                    classMaster.ClassId, classMaster.ClassName, classMaster.ClassDescription, classMaster.ClassPrice);
+            }
+
+            Console.Write("Done.");
+            Console.ReadLine();
+        }
+    }
+}
